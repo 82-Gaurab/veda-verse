58b62cfda5943e66057245ee178e23729c0cbecb
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-11 18:30:20 +0545

    Create bottom navigation bar

---
+++ lib/screen/bottom_navigation_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dashboard")),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_sharp),
+            label: "Wishlist",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Wishlist"),
+        ],
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vedaverse/screen/bottom_navigation_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,30 +7,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsetsGeometry.all(13),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // SizedBox(height: 40),
-                // Row(
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //   children: [
-                //     Image.asset("assets/images/title.png", width: 180),
-                //     Spacer(),
-                //     Icon(Icons.search, size: 40),
-                //     SizedBox(width: 10),
-                //     Icon(Icons.notifications, size: 40),
-                //   ],
-                // ),
-                Text("Welcome to Home Screen", style: TextStyle(fontSize: 30)),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: BottomNavigationScreen(),
+      // body: SafeArea(
+      //   child: Padding(
+      //     padding: EdgeInsetsGeometry.all(13),
+      //     child: Center(
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           // SizedBox(height: 40),
+      //           // Row(
+      //           //   crossAxisAlignment: CrossAxisAlignment.center,
+      //           //   children: [
+      //           //     Image.asset("assets/images/title.png", width: 180),
+      //           //     Spacer(),
+      //           //     Icon(Icons.search, size: 40),
+      //           //     SizedBox(width: 10),
+      //           //     Icon(Icons.notifications, size: 40),
+      //           //   ],
+      //           // ),
+      //           Text("Welcome to Home Screen", style: TextStyle(fontSize: 30)),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
