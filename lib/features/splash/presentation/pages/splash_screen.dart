ddf71dbeaed0c51b973439c6ce35e2741504d195
GAURAB <gaurabkhadka4082@gmail.com>
2026-01-15 06:58:32 +0545

    check for saved user session before navigation

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -1,26 +1,41 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vedaverse/core/services/storage/user_session_service.dart';
+import 'package:vedaverse/features/dashboard/presentation/pages/dashboard_screen.dart';
 import 'package:vedaverse/features/splash/presentation/pages/second_screen.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  Future<void> _navigateToNext() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+    // Note: Check if the user is already logged in
+    final userSessionService = ref.read(userSessionServiceProvider);
+    final isLoggedIn = userSessionService.isLoggedIn();
+
+    if (isLoggedIn) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => DashboardScreen()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SecondScreen()),
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 4), () {
-      if (context.mounted) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => SecondScreen()),
-        );
-      }
-    });
+    _navigateToNext();
   }
 
   @override
